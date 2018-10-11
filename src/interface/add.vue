a268e5018624d550a64e28f94858e4887d915084
毛慧慧 <huihui.mao@dbappsecurity.com.cn>
2018-10-11 10:55:42 +0800

    新增： 新增add.vue

---
+++ src/interface/add.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    新增文件
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+<style scoped>
+</style>
